431ffffe86343390baffdb679c3300b98c2715a5
Mali-zi <mne310872@yandex.ru>
2023-12-16 10:38:43 +0300

    init: init a project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="container-fluid">
+      <div className="row mt-4">
+        <div className="col m-4 cover">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
